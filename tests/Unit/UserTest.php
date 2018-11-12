dec0ff538b1e28525812a9b3aee58ce549e6ac8f
toral <tj244@njit.edu>
2018-11-11 23:10:05 -0500

    Task: Created UserTest for questions

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class UserTest extends TestCase
+{
+    public function testSave()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue($user->save());
+    }
+    public function testQuestions()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->questions()->get()));
+    }
+}
