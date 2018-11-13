728c1b3a5fd8cf643b4431b157e357b895514970
toral <tj244@njit.edu>
2018-11-13 12:13:08 -0500

    Task:Added a list of questions and a pager in homepage

---
+++ database/seeds/QuestionsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuestionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = App\User::all();
+        for ($i = 1; $i <= 5; $i++) {
+            $users->each(function ($user) {
+                $question = factory(\App\Question::class)->make();
+                $question->user()->associate($user);
+                $question->save();
+            });
+        }
+    }
+}
