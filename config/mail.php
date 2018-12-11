3974dd27556bd9ff6efdf10cd08822b278e60ecb
toral <tj244@njit.edu>
2018-12-10 20:49:36 -0500

    Fix: heroku error

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'myemail@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'toraljoshi0501@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME','my email'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD','my password'),
 
     /*
     |--------------------------------------------------------------------------
